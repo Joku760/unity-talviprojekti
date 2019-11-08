4b35deca9ee82f74ea438bbf548557906807c6d5
teppo223 <t6hote00@students.oamk.fi>
2019-11-08 13:59:35 +0200

    Yksinkertainen UI

---
+++ Assets/mainmenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainmenu : MonoBehaviour
+{
+    public void StartScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
+    }
+
+}
