5b65b44a9b954e8251fb5d1936268b4604aa47b4
Ilari <ilari.m97@gmail.com>
2019-11-14 14:52:51 +0200

    Highlight mouse over ominaisuus

---
+++ Assets/Scripts/HoverOutline.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverOutline : MonoBehaviour
+{
+    public Material border;
+    public Material nonBorder;
+    void OnMouseOver()
+    {
+        GetComponent<Renderer>().material = border;
+    }
+    void OnMouseExit()
+    {
+        GetComponent<Renderer>().material = nonBorder;
+    }
+}
