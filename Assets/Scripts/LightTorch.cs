68dca9e0ef9979eab0f5375b2688b97b00a93653
Ilari <ilari.m97@gmail.com>
2019-11-14 17:05:03 +0200

    Scriptit lapulle ja soihduille

---
+++ Assets/Scripts/LightTorch.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightTorch : MonoBehaviour
+{
+    public Transform player;
+    Light torchlight;
+
+    private void Start()
+    {
+        torchlight = GetComponentInChildren<Light>();
+        torchlight.gameObject.SetActive(false);
+    }
+    void FixedUpdate()
+    {
+        if (HaveLineOfSight())
+        {
+            torchlight.gameObject.SetActive(true);
+        }
+    }
+
+    bool HaveLineOfSight()
+    {
+        RaycastHit hit;
+        Vector3 direction = player.transform.position - transform.position;
+
+        if (Physics.Raycast(transform.position, direction, out hit, 3))
+        {
+            if (hit.transform.CompareTag("Player"))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ Assets/Scripts/ShowMessage.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowMessage : MonoBehaviour
+{
+    public GameObject objekti;
+    private void OnMouseDown()
+    {
+        objekti.gameObject.SetActive(true);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.F))
+        {
+            objekti.gameObject.SetActive(false);
+        }
+    }
+}
