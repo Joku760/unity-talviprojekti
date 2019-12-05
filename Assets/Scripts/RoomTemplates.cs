e7aa6baf1388a785a45c11e4171762a0eca0a87d
t7koto00 <43570793+t7koto00@users.noreply.github.com>
2019-12-05 15:11:19 +0200

    Huoneet listataan, viimeinen huone merkattu väliaikaisella merkillä

--- Assets/Scripts/RoomTemplates.cs
+++ Assets/Scripts/RoomTemplates.cs
@@ -10,4 +10,23 @@ public class RoomTemplates : MonoBehaviour
     public GameObject[] leftRooms;
     public GameObject[] rightRooms;
     public GameObject closedRoom;
+
+    public List<GameObject> rooms;
+
+    public float waitTime;
+    private bool spawnedBoss;
+    public GameObject boss;
+
+    void Update()
+    {
+        if(waitTime <= 0 && spawnedBoss == false)
+        {
+            Instantiate(boss, rooms[rooms.Count - 1].transform.position, Quaternion.identity);
+            spawnedBoss = true;
+        }
+        else
+        {
+            waitTime -= Time.deltaTime;
+        }
+    }
 }
